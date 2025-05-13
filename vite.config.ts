8812c408ce08c314091c5fc1c64588c743880bf5
David <rynstheoverlord@gmail.com>
2025-05-13 22:33:11 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,4 +32,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Add this alias for resolving @ to src
+    },
+  },
 })
